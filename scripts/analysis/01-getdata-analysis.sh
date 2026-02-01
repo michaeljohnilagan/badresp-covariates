603caf3efecfa9629e2b84da9e698f57e3999303
Michael Ilagan <michael.ilagan@mail.mcgill.ca>
2026-02-01 10:26:47 -0700

    put mkdir in getdata scripts

--- scripts/analysis/01-getdata-analysis.sh
+++ scripts/analysis/01-getdata-analysis.sh
@@ -1,3 +1,4 @@
 # real analysis data
+mkdir data
 wget https://osf.io/download/kv9xj/ -O './data/auxiliary-data-wide.csv'
 wget https://osf.io/download/hdmvs/ -O './data/experiments1&4-results-to-analysis.RDS'
--- scripts/simulation/01-getdata-simulation.sh
+++ scripts/simulation/01-getdata-simulation.sh
@@ -1,4 +1,5 @@
 # HSQ data
+mkdir data
 wget http://openpsychometrics.org/_rawdata/HSQ.zip
 unzip -j HSQ.zip HSQ/data.csv
 mv data.csv ./data/openpsychometrics-hsq.csv # rename
