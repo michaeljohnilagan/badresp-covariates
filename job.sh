6409cf15ef211238e1f12b1d9f8bd0edce87f3f1
Michael Ilagan <michael.ilagan@mail.mcgill.ca>
2026-02-18 17:04:35 -0700

    add file checks to run

--- job.sh
+++ job.sh
@@ -1,17 +1,31 @@
 # set up reproducible environment
-date; R -e 'renv::restore()'; date
+if [ ! -d './renv/library' ]; then
+R -e 'renv::restore()'
+fi
 
 # not simulation, not analysis
-date; R --no-save --no-restore --file=scripts/tests.R > tests.Rout 2>&1; date
-date; R --no-save --no-restore --file=scripts/figs-other.R > figs-other.Rout 2>&1; date
+R --no-save --no-restore --file=tests/tests.R > tests.Rout 2>&1
+R --no-save --no-restore --file=scripts/figs-other.R > figs-other.Rout 2>&1
 
-# simulation
+# get data for simulation
+if [ ! -f './data/openpsychometrics-hsq.csv' ]; then
 bash scripts/simulation/01-getdata-simulation.sh
-date; R --no-save --no-restore --file=scripts/simulation/02-simulation.R > 02-simulation.Rout 2>&1; date
-R --no-save --no-restore --file=scripts/simulation/03-figs-simulation.R > 03-figs-simulation.Rout 2>&1
+fi
 
-# analysis
+# run simulation
+if [ ! -f 'simulation.RData' ]; then
+R --no-save --no-restore --file=scripts/simulation/02-simulation.R > 02-simulation.Rout 2>&1
+R --no-save --no-restore --file=scripts/simulation/03-figs-simulation.R > 03-figs-simulation.Rout 2>&1
+fi
+
+# get data for analysis
+if [ ! -f './data/auxiliary-data-wide.csv' ] || [ ! -f './data/experiments1&4-results-to-analysis.RDS' ] ; then
 bash scripts/analysis/01-getdata-analysis.sh
-date; R --no-save --no-restore --file=scripts/analysis/02-analysis-preprocess.R > 02-analysis-preprocess.Rout 2>&1; date
-date; R --no-save --no-restore --file=scripts/analysis/03-analysis-main.R > 03-analysis-main.Rout 2>&1; date
+fi
+
+# run analysis
+if [ ! -f 'analysis.RData' ]; then
+R --no-save --no-restore --file=scripts/analysis/02-analysis-preprocess.R > 02-analysis-preprocess.Rout 2>&1
+R --no-save --no-restore --file=scripts/analysis/03-analysis-main.R > 03-analysis-main.Rout 2>&1
 R --no-save --no-restore --file=scripts/analysis/04-figs-analysis.R > 04-figs-analysis.Rout 2>&1
+fi
